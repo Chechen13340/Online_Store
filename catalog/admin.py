8f9f8bb22a60917dc33f63c7e19d6219678f611b
Artem Osipov <tema.osipov@mail.ru>
2023-10-01 19:18:53 +0300

    new settings and models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'cost', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'describe',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name',)
+
